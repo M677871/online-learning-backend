752ddca1c616b07eca5e4d892214fb148266db9b
M677871 <miledaz1234@gmail.com>
2026-02-08 09:49:43 +0200

    improve the messages of the response in the controllers

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,27 +1,28 @@
 const categoryService = require('../services/categoryService');
 const asyncHandler = require('../utils/asyncHandler');
+const categorySerializer = require('../serializers/categorySerializer');
 
 class CategoryController {
   static getAll = asyncHandler(async (req, res) => {
     const categories = await categoryService.getAllCategories();
-    res.json({ success: true, data: categories });
+    res.json({ success: true, data: categorySerializer.serializeList(categories) });
   });
 
   static getById = asyncHandler(async (req, res) => {
     const category = await categoryService.getCategoryById(req.params.id);
-    res.json({ success: true, data: category });
+    res.json({ success: true, data: categorySerializer.serialize(category) });
   });
 
   static create = asyncHandler(async (req, res) => {
     const { categoryName, description } = req.body;
     const category = await categoryService.createCategory({ categoryName, description });
-    res.status(201).json({ success: true, data: category, message: 'Category created successfully' });
+    res.status(201).json({ success: true, data: categorySerializer.serialize(category), message: 'Category created successfully' });
   });
 
   static update = asyncHandler(async (req, res) => {
     const { categoryName, description } = req.body;
     const category = await categoryService.updateCategory(req.params.id, { categoryName, description });
-    res.json({ success: true, data: category, message: 'Category updated successfully' });
+    res.json({ success: true, data: categorySerializer.serialize(category), message: 'Category updated successfully' });
   });
 
   static remove = asyncHandler(async (req, res) => {
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,32 +1,33 @@
 const userService = require('../services/userService');
 const asyncHandler = require('../utils/asyncHandler');
+const userSerializer = require('../serializers/userSerializer');
 
 class UserController {
   static getAll = asyncHandler(async (req, res) => {
     const users = await userService.getAllUsers();
-    res.json({ success: true, data: users });
+    res.json({ success: true, data: userSerializer.serializeList(users) });
   });
 
   static getById = asyncHandler(async (req, res) => {
     const user = await userService.getUserById(req.params.id);
-    res.json({ success: true, data: user });
+    res.json({ success: true, data: userSerializer.serialize(user) });
   });
 
   static getByEmail = asyncHandler(async (req, res) => {
     const user = await userService.getUserByEmail(req.params.email);
-    res.json({ success: true, data: user });
+    res.json({ success: true, data: userSerializer.serialize(user) });
   });
 
   static create = asyncHandler(async (req, res) => {
     const { email, password, userType } = req.body;
     const user = await userService.createUser({ email, password, userType });
-    res.status(201).json({ success: true, data: user, message: 'User created successfully' });
+    res.status(201).json({ success: true, data: userSerializer.serialize(user), message: 'User created successfully' });
   });
 
   static update = asyncHandler(async (req, res) => {
     const { email, password, userType } = req.body;
     const user = await userService.updateUser(req.params.id, { email, password, userType });
-    res.json({ success: true, data: user, message: 'User updated successfully' });
+    res.json({ success: true, data: userSerializer.serialize(user), message: 'User updated successfully' });
   });
 
   static remove = asyncHandler(async (req, res) => {
@@ -36,8 +37,8 @@ class UserController {
 
   static login = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-    const result = await userService.login(email, password);
-    res.json({ success: true, data: result });
+    const { token, user } = await userService.login(email, password);
+    res.json({ success: true, data: userSerializer.serializeAuth(token, user) });
   });
 
   static changePassword = asyncHandler(async (req, res) => {
